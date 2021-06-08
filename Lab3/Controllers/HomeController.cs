0270e5d53c3d58280e3b21b962794fe3bc4106e2
ajasa19 <test>
2021-06-08 14:54:20 -0400

    Inital commit of lab3

---
+++ Lab3/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Lab3.Models;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }  
+
+        [HttpGet]
+        [HttpPost]
+        public IActionResult Razor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Count()
+        {
+            HttpContext.Session.SetString("bottleNum", Request.Form["bottleNum"]);
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreatePerson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DisplayPerson(Person person)
+        {
+            return View(person);
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab3/Models/Person.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Models
+{
+    public class Person
+    {
+        [Required]
+        [StringLength(50)]
+        public string firstName
+        {
+            get; 
+            set;
+        }
+        
+        [Required]
+        [StringLength(50)]
+        public string lastName
+        {
+            get; 
+            set;
+        }
+
+        [Required]
+        public int age
+        {
+            get; 
+            set;
+        }
+        
+        [Required]
+        [StringLength(255)]
+        [EmailAddress]
+        public string emailAddress
+        {
+            get; 
+            set;
+        }
+        
+        [Required]
+        [DataType(DataType.Date)]
+        public string birthDate
+        {
+            get; 
+            set;
+        }
+        
+        [Required]
+        [StringLength(100)]
+        [BindProperty(Name = "pass")]
+        public string password
+        {
+            get; 
+            set;
+        }
+        
+        [Required]
+        [StringLength(255)]
+        public string description
+        {
+            get; 
+            set;
+        }
+    }
+}
