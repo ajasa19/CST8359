c656785acd05c5a88281039c72b505a1502c14ec
ajasa19 <test>
2021-06-20 15:03:01 -0400

    initial commit lab4

---
+++ Lab4/Models/CommunityMembership.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Models
+{
+    public class CommunityMembership
+    {
+
+        public int id { get; set; }
+
+        public int StudentId { get; set; }
+        public string CommunityId { get; set; }
+
+        /*dont know if following get/set calling to object is needed but is in git example*/
+        public Student Student { get; set; }
+        public Community Community { get; set; }
+
+
+    }
+}
