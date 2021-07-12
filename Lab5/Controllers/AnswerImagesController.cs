de557ad8a8bf82425e8618cd89dce46cb47975cc
ajasa19 <test>
2021-07-11 21:46:00 -0400

    Lab 5 inital commit & include .gitignore

---
+++ Lab5/Controllers/AnswerImagesController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Lab5.Data;
+using Lab5.Models;
+using Microsoft.AspNetCore.Http;
+using Azure.Storage.Blobs;
+using System.IO;
+using Azure;
+
+
+
+
+namespace Lab5.Controllers
+{
+    public class AnswerImagesController : Controller
+    {
+        private readonly AnswerImageDataContext _context;
+        private readonly BlobServiceClient _blobServiceClient;
+        private readonly string earthContainerName = "earthimages";
+	    private readonly string computerContainerName = "computerimages";
+
+        public AnswerImagesController(AnswerImageDataContext context, BlobServiceClient blobServiceClient)
+            {
+                _context = context;
+                _blobServiceClient = blobServiceClient;
+            }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.AnswerImages.ToListAsync());
+        }
+
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upload(IFormFile answerImage, IFormCollection IFormCollection)
+        {
+            BlobContainerClient containerClient;
+            string unasumingContainerName = "";
+            int questionInt = 0;
+
+            if (IFormCollection["Question"] == "0")
+            {
+                unasumingContainerName = computerContainerName;
+                questionInt = 0;
+            }
+            else
+            {
+                unasumingContainerName = earthContainerName;
+                questionInt = 1;
+            }
+
+            // Create the container and return a container client object
+            try
+            {
+                containerClient = await _blobServiceClient.CreateBlobContainerAsync(unasumingContainerName);
+                // Give access to public
+                containerClient.SetAccessPolicy(Azure.Storage.Blobs.Models.PublicAccessType.BlobContainer);
+            }
+            catch (RequestFailedException)
+            {
+                containerClient = _blobServiceClient.GetBlobContainerClient(unasumingContainerName);
+            }
+
+
+            try
+            {
+                // create the blob to hold the data
+                var blockBlob = containerClient.GetBlobClient(answerImage.FileName);
+                if (await blockBlob.ExistsAsync())
+                {
+                    await blockBlob.DeleteAsync();
+                }
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    // copy the file data into memory
+                    await answerImage.CopyToAsync(memoryStream);
+
+                    // navigate back to the beginning of the memory stream
+                    memoryStream.Position = 0;
+
+                    // send the file to the cloud
+                    await blockBlob.UploadAsync(memoryStream);
+                    memoryStream.Close();
+                }
+
+                // add the photo to the database if it uploaded successfully
+                var image = new AnswerImage();
+                image.Url = blockBlob.Uri.AbsoluteUri;
+                image.FileName = answerImage.FileName;
+                image.Question = questionInt;
+
+                _context.AnswerImages.Add(image);
+                _context.SaveChanges();
+            }
+            catch (RequestFailedException)
+            {
+                View("Error");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var image = await _context.AnswerImages
+                .FirstOrDefaultAsync(m => m.AnswerImageId == id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            return View(image);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id, IFormCollection IFormCollection)
+        {
+            var image = await _context.AnswerImages.FindAsync(id);
+            string unasumingContainerName = "";
+
+            if (IFormCollection["Question"] == "0")
+            {
+                unasumingContainerName = computerContainerName;
+            }
+            else
+            {
+                unasumingContainerName = earthContainerName;
+            }
+
+            BlobContainerClient containerClient;
+            // Get the container and return a container client object
+            try
+            {
+                containerClient = _blobServiceClient.GetBlobContainerClient(unasumingContainerName);
+            }
+            catch (RequestFailedException)
+            {
+                return View("Error");
+            }
+
+            try
+            {
+                // Get the blob that holds the data
+                var blockBlob = containerClient.GetBlobClient(image.FileName);
+                if (await blockBlob.ExistsAsync())
+                {
+                    await blockBlob.DeleteAsync();
+                }
+
+                _context.AnswerImages.Remove(image);
+                await _context.SaveChangesAsync();
+
+            }
+            catch (RequestFailedException)
+            {
+                return View("Error");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ Lab5/Models/AnswerImage.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab5.Models
+{
+    public enum Question
+    {
+        Earth, Computer
+    }
+    public class AnswerImage
+    {
+
+        public int AnswerImageId
+        {
+            get; 
+            set;
+        }
+
+        [Required]
+        [DisplayName("File Name")]
+        public string FileName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [Url]
+        public string Url
+        {
+            get;
+            set;
+        }
+
+        // [Required]
+        // public Question Question { get; set; }
+
+        [Required]
+        public int Question { get; set; }
+
+    }
+}
